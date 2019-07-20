79006f8b2f9b980b6a8f1ed64a3d05befead5fff
leoleil <leoleil@163.com>
2019-07-20 23:14:58 +0800

    ok

--- Project3/DowndataSocket.cpp
+++ Project3/DowndataSocket.cpp
@@ -129,6 +129,7 @@ int DowndataSocket::createReceiveServer(const int port, std::vector<message_buf>
 				closesocket(sClient);   //¹Ø±ÕÌ×½Ó×Ö
 				return -1;
 			}
+			Sleep(10);
 			memcpy(data_ptr, buf, retVal);
 			r_len = r_len + retVal;
 
