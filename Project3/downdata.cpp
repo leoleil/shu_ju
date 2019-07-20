3d3b628ea9e79082d0d7ff614aa07b04a29af04e
leoleil <leoleil@163.com>
2019-07-20 20:34:03 +0800

    提交

--- Project3/downdata.cpp
+++ Project3/downdata.cpp
@@ -8,13 +8,13 @@ DWORD download_rec(LPVOID lpParameter)
 	HANDLE hThread1;//创建数据解析线程，读取数据池中数据
 	hThread1 = CreateThread(NULL, 0, download, NULL, 0, NULL);
 	CloseHandle(hThread1);
-	DeleteCriticalSection(&data_CS);//删除关键代码段对象
+	
 	while (1) {
 		DowndataSocket service;//创建接收任务服务
 		service.createReceiveServer(4997, DATA_MESSAGES);
 	}
 	
-	
+	DeleteCriticalSection(&data_CS);//删除关键代码段对象
 	return 0;
 }
 
